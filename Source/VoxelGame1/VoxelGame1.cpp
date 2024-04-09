f972a6c899c8664848564c6f4e1a2cc3b079a0f3
Marius Mit <116459671+ElliotGreenwood@users.noreply.github.com>
2024-04-09 10:38:43 +0100

    startpush

---
+++ Source/VoxelGame1/VoxelGame1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VoxelGame1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VoxelGame1, "VoxelGame1" );
---
+++ Source/VoxelGame1/VoxelGame1GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "VoxelGame1GameModeBase.h"
+
